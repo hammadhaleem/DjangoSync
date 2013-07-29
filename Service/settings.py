9e809309a745a2828d5aa71d60c50168e519d21b
hammad haleem <hammadhaleem@gmail.com>
2013-07-29 23:26:48 +0530

    d

--- Service/settings.py
+++ Service/settings.py
@@ -134,7 +134,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'content',
     'product',
-    'dicussiom',
+    'dicussion',
 )
 
 # A sample logging configuration. The only tangible logging
