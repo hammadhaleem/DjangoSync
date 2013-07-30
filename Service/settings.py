6adb5bd6d22a210bd093892c957b79576f098199
hammad haleem <hammadhaleem@gmail.com>
2013-07-31 00:45:47 +0530

    added django.contrib.humanize

--- Service/settings.py
+++ Service/settings.py
@@ -137,6 +137,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django.contrib.humanize',
     'content',
     'product',
     'discussion',
