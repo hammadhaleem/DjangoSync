69e6ac1b92e3db5e75c0fbae6f5127545f7c7499
hammad haleem <hammadhaleem@gmail.com>
2013-08-03 00:50:49 +0530

    REgister with new admin

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from Service.admin import user_admin_site
 from product.models import *
 
 admin.site.register(item)
 admin.site.register(category)
+
+user_admin_site.register(item)
+user_admin_site.register(category)
