6b2917fc33bd67a36352f2effa66cdf30544230f
hammad haleem <hammadhaleem@gmail.com>
2013-08-02 11:04:41 +0530

    Urls edits

--- product/urls.py
+++ product/urls.py
@@ -6,11 +6,11 @@ from product.views import *
 
 urlpatterns = patterns('',
     url(r'^$',ItemIndex),
-    url(r'^item/edit/(?P<content_slug>\w+)/$',edit),
-   	url(r'^item/view/(?P<content_slug>\w+)/$',view),
-    url(r'^item/analytics',analytics),
-    url(r'^item/create',add),
-    url(r'^item/delete/(?P<content_slug>\w+)/$',remove),
+    url(r'^edit/(?P<content_slug>\w+)/$',edit),
+   	url(r'^view/(?P<content_slug>\w+)/$',view),
+    url(r'^analytics',analytics),
+    url(r'^create',add),
+    url(r'^delete/(?P<content_slug>\w+)/$',remove),
 
 )
 
