d9b86ec399394dcbf5e952b83eaddc25955c1893
hammad haleem <hammadhaleem@gmail.com>
2013-08-02 15:00:32 +0530

    as

--- product/urls.py
+++ product/urls.py
@@ -7,11 +7,11 @@ from product.views import *
 
 urlpatterns = patterns('',
     url(r'^$',ItemIndex),
-    url(r'^edit/(?P<content_slug>\w+\D+)/$',edit),
-   	url(r'^view/(?P<content_slug>\w+\D+)/$',view),
+    url(r'^edit/(?P<content_slug>\w+)/$',edit),
+   	url(r'^view/(?P<content_slug>\w+)/$',view),
     url(r'^analytics',analytics),
     url(r'^create',add),
-    url(r'^delete/(?P<content_slug>\w+\D+)/$',remove),
+    url(r'^delete/(?P<content_slug>[\w\D]+)/$',remove),
 
 )
 
