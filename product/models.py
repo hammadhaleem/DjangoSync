1b975b65a9166f54c4a1840e9d06b8e14c16a566
hammad haleem <hammadhaleem@gmail.com>
2013-08-02 14:40:31 +0530

    edits

--- product/models.py
+++ product/models.py
@@ -3,28 +3,28 @@ from django.db import models
 
 
 class category(models.Model):
-    id=models.AutoField(primary_key=True)
-    cat_name=models.CharField(max_length=200)
-    cat_desc=models.TextField()
-    cat_slug=models.CharField(max_length=200)
+    self.id=models.AutoField(primary_key=True)
+    self.cat_name=models.CharField(max_length=200)
+    self.cat_desc=models.TextField()
+    self.cat_slug=models.CharField(max_length=200)
 
     def __unicode__(self):
         return self.cat_slug
 
-        
+
 class item(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=200)
-    slug=models.CharField(max_length=200)
-    text=models.TextField()
-    created_by=models.CharField(max_length=200)
-    timestamp=models.DateTimeField('date published')
-    tags=models.CharField(max_length=400)
-    photo=models.CharField(max_length=200)
-    video=models.CharField(max_length=200)
-    time_to_deliver=models.IntegerField(default=1) #In days 
-    cost=models.IntegerField(default=1) #In Rupees 
-    category_id=models.ForeignKey(category.id) #Key 
+    self.id=models.AutoField(primary_key=True)
+    self.name=models.CharField(max_length=200)
+    self.slug=models.CharField(max_length=200)
+    self.text=models.TextField()
+    self.created_by=models.CharField(max_length=200)
+    self.timestamp=models.DateTimeField('date published')
+    self.tags=models.CharField(max_length=400)
+    self.photo=models.CharField(max_length=200)
+    self.video=models.CharField(max_length=200)
+    self.time_to_deliver=models.IntegerField(default=1) #In days 
+    self.cost=models.IntegerField(default=1) #In Rupees 
+    self.category_id=models.ForeignKey(category.id) #Key 
     #models.IntegerField(default=0)
 
     def __unicode__(self):
