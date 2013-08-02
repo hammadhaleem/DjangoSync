ad9ec15e9bf180ed38e19e28ed9ea993336c3cd0
hammad haleem <hammadhaleem@gmail.com>
2013-08-02 15:11:22 +0530

    sd

--- Service/views.py
+++ Service/views.py
@@ -5,6 +5,8 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from django.shortcuts import render, render_to_response
 from content.models import Article
+from product.models import *
+
 
 def about(request):
 	return render(request , 'about.html')
@@ -20,4 +22,5 @@ def content(request, content_slug):
     return render(request , 'blog.html' , {'name':art[0].name , 'text':art[0].text})
 
 def inventory(request):
-	return render(request , 'inventory.html')
+	ite =item.objects.all()
+	return render(request , 'inventory.html' , ite )
