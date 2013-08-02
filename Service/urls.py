ae267321f11139d926f90782108c997f96104676
hammad haleem <hammadhaleem@gmail.com>
2013-08-03 00:22:09 +0530

    a

--- Service/urls.py
+++ Service/urls.py
@@ -25,6 +25,7 @@ urlpatterns = patterns('',
     (r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    
     url(r'^', include(user_admin_site.urls)),
     url(r'^', include('myapp.urls')),
 )
