db12517566eaec78407a4f4288c2337d073a0786
hammad haleem <hammadhaleem@gmail.com>
2013-08-03 00:27:38 +0530

    ugettext

--- Service/admin.py
+++ Service/admin.py
@@ -2,7 +2,8 @@ from django import forms
 from django.contrib import admin
 from django.contrib.admin.sites import AdminSite
 from django.contrib.auth.forms import AuthenticationForm
-
+from django.utils.translation import ugettext_lazy
+from django.utils.translation import *
  
 class UserAdminAuthenticationForm(AuthenticationForm):
     """
