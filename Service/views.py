abf7c37290fcd0992a18c7acbe07bfd1b2195bc7
hammad haleem <hammadhaleem@gmail.com>
2013-08-03 09:14:45 +0530

    abc

--- Service/views.py
+++ Service/views.py
@@ -1,12 +1,13 @@
-
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.shortcuts import render, render_to_response
-from content.models import Article
-from product.models import item 
-from product.models import category
+
+
+from content.models import *
+from product.models import *
+
 
 def about(request):
 	return render(request , 'about.html')
@@ -16,16 +17,16 @@ def index(request):
     
 def faq (request) :
 	return render(request , 'faq.html' )
-
 def content(request, content_slug):
+
     art = Article.objects.filter(slug=content_slug)
     return render(request , 'blog.html' , {'name':art[0].name , 'text':art[0].text})
 
-def inventory(request):
-	ite =item.objects.all()
-	return render(request , 'inventory.html' , {'ite':ite} )
 
 def category(request):
 	cat =category.objects.all()
 	return render(request , 'inventory.html' , {'cat':cat} )
 
+def inventory(request):
+	ite =item.objects.all()
+	return render(request , 'inventory.html' , {'ite':ite} )
