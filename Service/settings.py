99bcbe4829c5705c1895d2f05b369bec5fe566fe
hammad haleem <hammadhaleem@gmail.com>
2013-08-12 23:13:25 +0530

    a

--- Service/settings.py
+++ Service/settings.py
@@ -121,6 +121,7 @@ TEMPLATE_DIRS = (
     SITE_ROOT + "templates" ,
 )
 AUTH_PROFILE_MODULE = 'Service.UserProfile'
+
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.request",
