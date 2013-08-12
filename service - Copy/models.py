3ed1b7f1e581d97efa34364882f914f48346c2d7
hammad haleem <hammadhaleem@gmail.com>
2013-08-12 23:38:55 +0530

    f

--- service - Copy/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-     
-     
-class UserProfile(models.Model):
-    url = models.URLField()
-    home_address = models.TextField()
-    phone_numer = models.TextField()
-    user = models.ForeignKey(User, unique=True)
