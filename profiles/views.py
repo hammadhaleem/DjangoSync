e00300d00b71a876b523bdf26620a3be782e88ce
hammad haleem <hammadhaleem@gmail.com>
2013-08-12 23:01:21 +0530

    s

--- profiles/views.py
+++ profiles/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.views.generic.list_detail import ListView as object_list
+from django.views.generic.list import ListView as object_list
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
