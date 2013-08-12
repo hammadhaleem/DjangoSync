4a380e97e0f085c4790e179a7ae0af2e184f2829
hammad haleem <hammadhaleem@gmail.com>
2013-08-12 21:17:19 +0530

    Template

--- Service/views.py
+++ Service/views.py
@@ -53,7 +53,8 @@ def about(request):
 	return render(request , 'about.html')
 
 def index(request):
-    return render(request , 'index.html')
+    ite =item.objects.all()
+    return render(request , 'index.html' ,  {'ite':ite })
     
 def faq (request) :
 	return render(request , 'faq.html' )
