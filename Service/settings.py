7f2e1ad85e153f5cae33bb9f8bcf7ad123cb2854
hammad haleem <hammadhaleem@gmail.com>
2013-08-12 22:34:01 +0530

    sa
    s

--- Service/settings.py
+++ Service/settings.py
@@ -13,8 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django_test', 
-                     # Or path to database file if using sqlite3.
+        'NAME': 'Service',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'kgggdkp2692',
